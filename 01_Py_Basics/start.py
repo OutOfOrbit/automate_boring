d36910a0eaa5168e20514ae3e1b5914ae150cbb1
OutOfOrbit <ronromaniuk@hotmail.com>
2022-02-06 00:18:13 -0500

    mini-commit

---
+++ 01_Py_Basics/start.py
@@ -0,0 +1,10 @@
+# 1st starter program in the "Automate Boring book/course"
+print('\nHOLLOW WORLD\n')
+print("What's your name, dufus? ")
+my_name = input()
+print(f"\nNice to meed u, {my_name.title()}")
+print(f"The length of your name is {len(my_name.title())}")
+print("\nWhat's your age? ")
+my_age = input()
+print(f"\nOk {my_name.title()}, you'll be {str(int(my_age) + 1)} in a year.")
+#
